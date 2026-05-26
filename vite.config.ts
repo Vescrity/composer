2035a7ace61ac0d1fc17ce780ae9707c79b45d85
雪叶 <93505720+Vescrity@users.noreply.github.com>
2026-05-26 21:44:10 +0800

    workflow

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: process.env.VITE_BASE_PATH || './', // Optimized to resolve relatively for static servers/GitHub Pages
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
